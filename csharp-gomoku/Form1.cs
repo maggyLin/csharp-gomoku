03e1a757f30c1bbbf4113f6beea6dd4e12273e07
maggylin_acer <maggylin0317@gmail.com>
2022-03-15 21:09:18 +0800

    new piece class

---
+++ csharp-gomoku/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csharp_gomoku
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Controls.Add(new BlackPiece(10, 10));
+            this.Controls.Add(new WhitePiece(100, 100));
+        }
+    }
+}
---
+++ csharp-gomoku/Piece.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace csharp_gomoku
+{
+    abstract class Piece : PictureBox
+    {
+        public Piece(int x, int y)
+        {
+            this.BackColor = Color.Transparent; //使用 System.Drawing.Color 指定背景顏色
+            this.Location = new Point(x, y);
+            this.Size = new Size(50, 50);
+        }
+
+    }
+}
